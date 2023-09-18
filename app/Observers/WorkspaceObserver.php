f9723b983fce8f432900f5ba41ed68f683749dd9
Milad <miladfathi021@gmail.com>
2023-09-18 23:29:07 +0330

    Created workspace observer

---
+++ app/Observers/WorkspaceObserver.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class WorkspaceObserver
+{
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        $user->workspaces()->create([
+            'name' => $this->request->get('workspace')
+        ]);
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}

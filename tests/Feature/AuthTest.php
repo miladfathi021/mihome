11f237d66caaf26dd4ef0ac460e9e0911265c1ec
Milad <miladfathi021@gmail.com>
2023-09-18 23:29:19 +0330

    Created auth test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -14,10 +15,37 @@ class AuthTest extends TestCase
     public function guest_can_create_a_new_account()
     {
         $this->withoutExceptionHandling();
+        Artisan::call('passport:install');
 
         $data = [
             'name' => 'Milad Fathi',
-            'email' => 'miladfathi021@gmail.com'
+            'email' => 'miladfathi021@gmail.com',
+            'phone' => '',
+            'password' => 'password',
+            'workspace' => 'parsa'
         ];
+
+        $this->assertDatabaseCount('users', 0);
+        $this->assertDatabaseCount('workspaces', 0);
+
+        $this->postJson(route('signup'), $data)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'token'
+                ],
+                'message'
+            ]);
+
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email']
+        ]);
+
+        $this->assertDatabaseCount('workspaces', 1);
+        $this->assertDatabaseHas('workspaces', [
+            'name' => $data['workspace']
+        ]);
     }
 }

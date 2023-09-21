d5432c589f951967ed287b9a98c2c514bbe9b000
Milad <miladfathi021@gmail.com>
2023-09-21 13:29:35 +0330

    using service instead of observer

--- app/Http/Controllers/V1/Auth/SignupController.php
+++ app/Http/Controllers/V1/Auth/SignupController.php
@@ -5,24 +5,21 @@ namespace App\Http\Controllers\V1\Auth;
 use App\Http\Controllers\ApiController;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
+use App\Services\AuthService;
 
 class SignupController extends ApiController
 {
     /**
      * @param \App\Http\Requests\SignupRequest $request
+     * @param \App\Services\AuthService        $authService
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(SignupRequest $request) : \Illuminate\Http\JsonResponse
+    public function __invoke(SignupRequest $request, AuthService $authService) : \Illuminate\Http\JsonResponse
     {
-        $data = $request->all();
-        $data['password'] = bcrypt($data['password']);
+        $user = $authService->createUser($request->all());
 
-        $user = User::create($data);
-        // After user created the WorkspaceObserver makes a new workspace
-
-        // Create token for user
-        $token = $user->createToken('API Token')->accessToken;
+        $token = $authService->createToken($user);
 
         return $this->responseOk([
             'token' => $token

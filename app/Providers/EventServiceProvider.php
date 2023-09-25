1efe9f80a9ffde658dd45ce55f167bbb7649c0e6
Milad <miladfathi021@gmail.com>
2023-09-25 11:30:39 +0330

    remove workspace observer from event service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,12 +2,9 @@
 
 namespace App\Providers;
 
-use App\Models\User;
-use App\Observers\WorkspaceObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -20,10 +17,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-    ];
-
-    protected $observers = [
-        User::class => [WorkspaceObserver::class]
     ];
 
     /**

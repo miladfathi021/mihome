648ef8030957b7b33684cd82ab26ea912184da0d
Milad <miladfathi021@gmail.com>
2023-09-28 18:07:09 +0330

    Add signin route

--- routes/Auth/auth.php
+++ routes/Auth/auth.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\V1\Auth\SigninController;
 use App\Http\Controllers\V1\Auth\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('signup', SignupController::class)
     ->name('signup');
+
+Route::post('signin', SigninController::class)
+    ->name('signin');

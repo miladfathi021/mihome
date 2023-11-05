0b92e2ca20f857866d01cbea5aefdc5dec255007
Milad <miladfathi021@gmail.com>
2023-11-05 12:59:22 +0330

    Create User Controller

---
+++ app/Http/Controllers/V1/Workspace/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\V1\Workspace;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\InvitationRequest;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(InvitationRequest $request)
+    {
+
+    }
+}
